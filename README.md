c97f256dd42cc3796b17eb22c27479876f2f668b
Alexander M Scheurer <alexandermscheurer@gmail.com>
2017-12-29 17:31:29 +0100

    Add invite functionality

--- README.md
+++ README.md
@@ -10,9 +10,15 @@ deres profil oplysninger og hvad folk ellers kan finde på til mig.
 #### Hvad kan jeg nu?:
 - [x] Poste i annonceringer hver gang en ny kanal åbnes.
 - [x] Byde nye medlemmer velkommen
-- [ ] Tjekke om folk har udfyldt deres profiloplysninger
+- [x] Tjekke om folk har udfyldt deres profiloplysninger
+- [ ] Sende invitationer til nye medlemmer
 
-
+#### Hvad har jeg brug for?
+- En bot api token
+- En user api token (legacy token)
+- Slack skal kende min webadresse
+Under "Interactive Components" skal webadressen jeg bor på indtastes med "/command" efter.
+På den måde, kan Slack tale med mig.
 
 #### Har du lyst til at kode på mig?
 Så kod i filen `lib/robot-pirat.js`, robotpiraten benyttter sig af dette
--- bot.js
+++ bot.js
@@ -1,12 +1,25 @@
-#!/usr/local/bin/node
+#!/usr/bin/env node
 "use strict";
-
 const path = require('path')
-const pirateBot = require(path.join(__dirname, 'lib', 'robot-pirat'));
+const fs   = require('fs')
 
+const pirateBot  = require(path.join(__dirname, 'lib', 'robot-pirat'));
+const roboServer = require(path.join(__dirname, 'lib', 'server'));
 
-const token = process.env.BOT_API_KEY;
-const name  = process.env.BOT_NAME;
-const roboPirate = new pirateBot({token, name, channel: 'annonceringer'});
+const token        = process.env.BOT_API_KEY;
+const userToken    = process.env.USER_API_KEY;
+const name         = process.env.BOT_NAME;
+const PORT         = process.env.PORT;
+const joinChannels = JSON.parse(
+	fs.readFileSync(path.join(__dirname, 'messages', 'joinChannels.json')));
 
+const roboPirate = new pirateBot({
+    token,
+    name,
+    channel: 'annonceringer',
+    inviteChannel: 'testrobot'
+});
+const server = new roboServer({PORT, roboPirate, userToken, joinChannels});
+
+server.run();
 roboPirate.run();
